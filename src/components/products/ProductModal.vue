adfa4d28ca7ec3a4f3f2993e30d66f18f558c2f9
으니 <72530436+eundol0519@users.noreply.github.com>
2024-04-24 23:29:52 +0900

    📝 : Vue Practice

--- src/components/products/ProductModal.vue
+++ src/components/products/ProductModal.vue
@@ -2,10 +2,13 @@
   <div class="black-bg" v-if="modal.showYN">
     <div class="white-bg">
       <img :src="products[this.modal.index].image" alt="room" />
+      <br />
       <span>상품 번호 : {{ products[this.modal.index].id }} / 허위 매물 신고 수 : {{ products[this.modal.index].count }}건</span>
       <h4>{{ products[this.modal.index].title }}</h4>
       <p>{{ products[this.modal.index].content }}</p>
-      <p>{{ products[this.modal.index].price }}원</p>
+      <!-- <input v-on:input="months = $event.target.value" :value="months" placeholder="개월 수를 입력해주세요" /> -->
+      <input v-model="months" placeholder="개월 수를 입력해주세요" />
+      <p>{{ this.months || 0 }} * {{ products[this.modal.index].price }} = {{ this.months * products[this.modal.index].price }}원</p>
       <button @click="modalClose">모달창 닫기</button>
     </div>
   </div>
@@ -31,11 +34,21 @@ export default {
   data() {
     return {
       products: productsData,
+      months: 1,
     };
   },
   methods: {
     modalClose() {
       this.$emit("modalShowHandler", false);
+    },
+  },
+  watch: {
+    months(value) {
+      if (isNaN(value)) {
+        alert("숫자만 입력해주세요.");
+        this.months = 1;
+        return;
+      }
     },
   },
 };
@@ -51,7 +64,7 @@ div {
 }
 
 img {
-  width: 100%;
+  width: 500px;
 }
 
 .black-bg {
@@ -60,10 +73,14 @@ img {
   background: rgba(0, 0, 0, 0.5);
   position: fixed;
   padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .white-bg {
-  width: 100%;
+  width: fit-content;
+  height: fit-content;
   background: white;
   border-radius: 8px;
   padding: 20px;
