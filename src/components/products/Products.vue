e69e4f6ba4fc7316cbd3fadad05065ce30ab8bf7
으니 <72530436+eundol0519@users.noreply.github.com>
2024-04-25 22:32:25 +0900

    📝 : Sort Practice(String, Number)

--- src/components/products/Products.vue
+++ src/components/products/Products.vue
@@ -1,9 +1,14 @@
 <template>
+  <button @click="sortHandler('price')">가격 순으로 정렬</button>
+  &nbsp;
+  <button @click="sortHandler('title')">제목 순으로 정렬</button>
+  &nbsp;
+  <button @click="sortHandler">되돌리기</button>
   <div>
     <h1>원룸샵</h1>
     <div class="room" :key="i" v-for="(product, i) in products">
       <img :src="product.image" alt="room" />
-      <h4>{{ product.name }}</h4>
+      <h4>{{ product.title }}</h4>
       <p>{{ product.price }} 만원</p>
       <button @click="changeCounter(i)">허위 매물 신고</button>&nbsp;
       <span>신고 수 : {{ product.count }}</span>
@@ -31,6 +36,19 @@ export default {
     modalHandler(index) {
       this.$emit("setIndex", index);
       this.$emit("modalShowHandler", true);
+    },
+    sortHandler(sortType) {
+      switch (sortType) {
+        case "title": // 문자열 정렬
+          this.products = [...this.products].sort((a, b) => (a.title < b.title ? -1 : a.title > b.title ? 1 : 0));
+          break;
+        case "price": // 숫자 정렬
+          this.products = [...this.products].sort((a, b) => a.price - b.price);
+          break;
+        default:
+          this.products = productsData;
+          break;
+      }
     },
   },
 };
